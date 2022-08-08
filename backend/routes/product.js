7db958040db165ded739dc817c0ad144ab04494f
saeed <saeednabati@hotmail.com>
2022-08-08 19:44:24 +0430

    reviews api

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { route } = require('../app');
 const router = express.Router();
-const {getProducts,newProduct,getSingleProduct,updateProduct,deleteProduct} = require('../controllers/productController')
+const {getProducts,newProduct,getSingleProduct,updateProduct,deleteProduct,createProductReview,getProductReviews,deleteReview} = require('../controllers/productController')
 const {isAuthenticatedUser,authorizeRoles} = require('../middlewares/auth');
 router.route('/products').get(getProducts)
 
@@ -10,4 +10,8 @@ router.route('/product/new').post(isAuthenticatedUser,authorizeRoles('admin'),ne
 router.route('/product/:id').get(getSingleProduct)
 
 router.route('/product/:id').put(isAuthenticatedUser,authorizeRoles('admin'),updateProduct).delete(isAuthenticatedUser,authorizeRoles('admin'),deleteProduct);
+
+router.route('/review').put(isAuthenticatedUser,createProductReview);
+router.route('/reviews').get(isAuthenticatedUser,getProductReviews);
+router.route('/reviews').delete(isAuthenticatedUser,deleteReview);
 module.exports = router;
