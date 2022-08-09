d46a817a364257095e12a31ea28ca10d53dee449
saeed <saeednabati@hotmail.com>
2022-08-09 22:33:42 +0430

    redux - productDetails

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,30 @@ import {
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import Home from './components/Home';
+import ProductDetails from './components/product/ProductDetails';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
       <div className="App">
      <Header/>
      <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="/product/:id" element={<ProductDetails/>} />
      </Routes>
     <Footer/>
+    <ToastContainer
+    position="top-right"
+    autoClose={5000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    />
     </div>
    
   );
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,10 @@
 import {createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
-
+import {productsReducer,productDetailsReducer} from '././productReducers'
 const reducer = combineReducers({
+    products: productsReducer,
+    productDetails: productDetailsReducer
 
 })
 
